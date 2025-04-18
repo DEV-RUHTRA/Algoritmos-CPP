2af9a02c7311170a506d083ec8c563ba722b42f1
Arthur do Nascimento Rodrigues <atrodrigues2015@gmail.com>
2025-04-18 12:14:25 -0300

    Estudando

--- Estudos/Comando de loop Do While.cpp
+++ Estudos/Comando de loop Do While.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main(){
 
+    int numero;
+
+    do{
+        cout << "Digite um numero mair que zero: ";
+        cin >> numero;
+        
+    } while (numero <= 0);
+    cout << "Seu numero e:" << numero << endl;
     
 
     system ("pause");
---
+++ Exercícios/Exercício de loop.cpp
@@ -0,0 +1,10 @@
+/*
+Escreva uma funcao que receba um valor inteiro N e retorne a soma de todos os ńumeros
+ımpares de 1 ate N. Para essa questao nao  ́e necessario fazer funcao principal (main).
+
+Exemplos:
+Entrada Sa ́ıda
+10 25
+30 225
+*/
+
