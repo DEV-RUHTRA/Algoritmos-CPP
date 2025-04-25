d76a95fb322cf54e70c3d0b510c48e058c48ebdb
Arthur do Nascimento Rodrigues <atrodrigues2015@gmail.com>
2025-04-25 11:47:48 -0300

    Estudando

--- Estudos/Comando de loop FOR.cpp
+++ Estudos/Comando de loop FOR.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main(){
     
-    
+    for(int x = 0; x <=20; x+=2){ //For estabelece uma variavel de partida, uma condição e um inc ou dec.
+                                 //Pode ser usada para varios loop's.                                 
+        cout << x << endl;
+    }
 
     system("pause");
     return 0;
